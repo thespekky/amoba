2320e68c8cf1c5c454c22e864dc82d472e1683c6
thespekky <bandras123456@gmail.com>
2021-10-06 09:39:48 +0200

    generálás elkezdése

--- Amoba_Bodnar_Tesok/Amoba_Bodnar_Tesok/Form1.Designer.cs
+++ Amoba_Bodnar_Tesok/Amoba_Bodnar_Tesok/Form1.Designer.cs
@@ -29,13 +29,115 @@ namespace Amoba_Bodnar_Tesok
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Name1 = new System.Windows.Forms.TextBox();
+            this.Name2 = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Első játékos neve:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Második játékos neve:";
+            // 
+            // Name1
+            // 
+            this.Name1.Location = new System.Drawing.Point(191, 33);
+            this.Name1.Name = "Name1";
+            this.Name1.Size = new System.Drawing.Size(100, 20);
+            this.Name1.TabIndex = 2;
+            // 
+            // Name2
+            // 
+            this.Name2.Location = new System.Drawing.Point(191, 66);
+            this.Name2.Name = "Name2";
+            this.Name2.Size = new System.Drawing.Size(100, 20);
+            this.Name2.TabIndex = 3;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(332, 33);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(118, 46);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "O";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(114, 370);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 31);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "O";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(912, 450);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Name2);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Amőba";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Name1;
+        private System.Windows.Forms.TextBox Name2;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button16;
     }
 }
 
--- Amoba_Bodnar_Tesok/Amoba_Bodnar_Tesok/Form1.cs
+++ Amoba_Bodnar_Tesok/Amoba_Bodnar_Tesok/Form1.cs
@@ -12,9 +12,41 @@ namespace Amoba_Bodnar_Tesok
 {
     public partial class Form1 : Form
     {
+        static Button[,] gombtomb = new Button[10,10];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //40; 340   40; 370     114; 370    méret:75; 31
+            jatektergeneralas();
+        }
+
+        private void jatektergeneralas()
+        {
+            int seged = 0;
+            for (int i = 0; i <10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    seged++;
+                    Button button = new Button();
+                    button.Location = new System.Drawing.Point((40 + (j * 75) + 20), (370 - (j * 30)));
+                    button.Name = "label"+seged;
+                    button.Text = "";
+                    button.Visible = true;
+                    button.Size = new System.Drawing.Size(75, 31);
+                    button.BackColor = Color.LightBlue;
+                    button.TextAlign = ContentAlignment.MiddleCenter;
+                    Controls.Add(button);
+                    button.BringToFront();
+                    button.Tag = "0";
+                    gombtomb[i, j] = button;
+                }
+            }
+            
         }
     }
 }
